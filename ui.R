8db5b2dffa6604a229572edae455b72408603eee
Gerard <gerard@whyzehealth.com>
2025-02-03 12:01:17 +0000

    initialising repo

--- ui.R
+++ ui.R
@@ -1,25 +1,24 @@
-navbarPage(
+library(shiny)
+library(bslib)
+library(DT)
+
+ui <- navbarPage(
   "Synthetic Data Generator",
   id = "mainNav",
   theme = bs_theme(),
   
-  # 1) Variables Setup
   tabPanel(
     "Variables Setup",
     fluidPage(
       br(),
-      strong("Use 'Add Variable' to define numeric or categorical variables. Then 
-             press 'Generate Data' to produce the dataset. View results in 'Data Summary' tab."),
-      br(),
-      actionButton("addVar", "Add Variable", icon = icon("plus"), class = "btn-primary"),
+      actionButton("btn_addVar", "Add Variable", icon=icon("plus"), class="btn-primary"),
       br(), br(),
-      uiOutput("varCards"),  # summaries for each variable
+      uiOutput("varCards"),
       br(),
-      actionButton("generateData", "Generate Data", icon=icon("cogs"), class="btn-success")
+      actionButton("btn_generate", "Generate Data", icon=icon("cogs"), class="btn-success")
     )
   ),
   
-  # 2) Data Summary
   tabPanel(
     "Data Summary",
     fluidPage(
